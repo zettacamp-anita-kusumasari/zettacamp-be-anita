6e8e99845ce10b30e82f654dc29ec0f24fe19ca0
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-08 22:13:30 +0700

    [ZettaCamp][Module 1][Project Setup & CRUD for Student and School Data] 1.3. Setup GraphQL Server with Express.js: Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 // *************** IMPORT CORE ***************
+require('dotenv').config(); 
 const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const connectDB = require('./config/database');
 const app = express();
+
+// ****************** Retrieve the MONGODB_URI value from environment
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// ****************** Setup PORT value from environment
 const PORT = process.env.PORT || 4000;
 
-// *************** Load environment variables from .env file
-dotenv.config();
-
-// *************** Connect to MongoDB using Mongoose with connection string from .env
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+// ****************** Calling the connectDB to connect the app with the MongoDB database
+connectDB(MONGODB_URI);
 
 // *************** Import Apollo Server middleware for Express
 const { ApolloServer } = require('apollo-server-express');
